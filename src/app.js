8e28cc696b023ded48883efc3e9c4305407f7ac3
ayaalakwaa <153412822+ayaalakwaa@users.noreply.github.com>
2024-02-06 05:32:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,4 +4,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//write your code here
+window.onload = function() {
+  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = ["com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extensions[Math.floor(Math.random() * extensions.length)]
+        );
+      }
+    }
+  }
+};
