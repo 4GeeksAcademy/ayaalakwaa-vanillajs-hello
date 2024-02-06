ed9fc775ce6cb0e39dbf0cb9567d194c9b540b1c
ayaalakwaa <153412822+ayaalakwaa@users.noreply.github.com>
2024-02-06 06:01:08 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
@@ -20,4 +12,3 @@ window.onload = function() {
       }
     }
   }
-};
