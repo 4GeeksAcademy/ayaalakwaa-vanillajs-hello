c185f0086bf85986a3f3b52a8b8561fdbeba4a5e
ayaalakwaa <153412822+ayaalakwaa@users.noreply.github.com>
2024-02-06 05:54:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-  let extensions = ["com", ".net", ".us", ".io"];
+  let extensions = [".com", ".net", ".us", ".io"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
