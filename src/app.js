d09cb09eab8db5ecfc0715a796ef62abd5cc752f
ayaalakwaa <153412822+ayaalakwaa@users.noreply.github.com>
2024-02-06 05:33:08 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -14,8 +14,7 @@ window.onload = function() {
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + extensions[Math.floor(Math.random() * extensions.length)]
-        );
+        console.log(pronoun[i] + adj[j] + noun[k] + extensions[Math.floor(Math.random() * extensions.length)]);
       }
     }
   }
